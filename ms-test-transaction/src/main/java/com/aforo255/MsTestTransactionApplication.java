02700222f99c9d24c6a364229790fd2019448743
Diego Castro <diego.castro@restituciondetierras.gov.co>
2021-09-21 19:51:33 -0500

    Proyecto ms-test-transaction!

---
+++ ms-test-transaction/src/main/java/com/aforo255/MsTestTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.aforo255;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTestTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTestTransactionApplication.class, args);
+	}
+
+}
